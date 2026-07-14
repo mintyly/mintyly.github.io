a4b2b96a7f94c8bdce14b11a6f5980726026a847
mintyly <ongjie90@gmail.com>
2026-07-14 16:08:59 +0800

    enhance SEO metadata; add Open Graph and Twitter Card tags for improved sharing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
 export default defineConfig({
+  site: 'https://mintyly.github.io',
   integrations: [icon()],
   markdown: {
     syntaxHighlight: 'shiki',
